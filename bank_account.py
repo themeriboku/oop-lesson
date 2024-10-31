441f7f46cc1d4ff43351b0d9045fe908debe218a
krittin kongsiang <krittin.ko@ku.th>
2024-10-31 14:01:01 +0700

    delete database

--- bank_account.py
+++ bank_account.py
@@ -1,32 +1,3 @@
-class AccountDB:
-    def __init__(self):
-        self.account_database = []
-
-    def insert(self, account):
-        index = self.__search_private(account.account_number)
-        if index == -1:
-            self.account_database.append(account)
-        else:
-            print(account, "Duplicated account; nothing to be insert")
-    
-    def __search_private(self, account_num):
-        for i in range(len(self.account_database)):
-            if self.account_database[i].account_number == account_num:
-                return i
-        return -1
-    
-    def search_public(self, account_num):
-        for account in self.account_database:
-            if account.account_number == account_num:
-                return account
-        return None
-    
-    def __str__(self):
-        s = ''
-        for account in self.account_database:
-            s += str(account) + ", "
-        return s
-        
 class Account:
     def __init__(self, num, type, account_name, balance):
         self.account_number = num
@@ -50,16 +21,10 @@ account2 = Account("0001", "checking", "John Hennessy", 2000)
 account3 = Account("0003", "saving", "Mark Hill", 3000)
 account4 = Account("0004", "saving", "David Wood", 4000)
 account5 = Account("0004", "saving", "David Wood", 4000)
-my_account_DB = AccountDB()
-my_account_DB.insert(account1)
-my_account_DB.insert(account2)
-my_account_DB.insert(account3)
-my_account_DB.insert(account4)
-my_account_DB.insert(account5)
-print(my_account_DB)
-my_account_DB.search_public("0003").deposit(50)
-print(my_account_DB)
-my_account_DB.search_public("0003").withdraw(100)
-print(my_account_DB)
-my_account_DB.search_public("0010").deposit(50)
-print(my_account_DB)
+print(account1)
+account1.deposit(1000)
+account2.withdraw(200)
+print(account2)
+print(account3)
+print(account4)
+print(account5)
