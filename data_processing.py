a64b0c92c4ee9c5348c128121956bd355e4e25ba
krittin kongsiang <krittin.ko@ku.th>
2024-10-31 14:53:49 +0700

    oop data processing

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,5 @@
 import csv, os
+import copy
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
@@ -15,96 +16,57 @@ with open(os.path.join(__location__, 'Countries.csv')) as f:
     for r in rows:
         countries.append(dict(r))
 
-# Print the average temperature of all the cities
-print("The average temperature of all the cities:")
-temps = []
-for city in cities:
-    temps.append(float(city['temperature']))
-print(sum(temps)/len(temps))
-print()
+class DB:
+    def __init__(self):
+        self.database = []
 
-# Print all cities in Italy
-cities_temp = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        cities_temp.append(city['city'])
-print("All the cities in", my_country, ":")
-print(cities_temp)
-print()
+    def insert(self, table):
+        self.database.append(table)
 
-# Print the average temperature for all the cities in Italy
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The average temperature of all the cities in", my_country, ":")
-print(sum(temps)/len(temps))
-print()
+    def search(self, table_name):
+        for table in self.database:
+            if table.table_name == table_name:
+                return table
+        return None
+    
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+    
+    def join(self, other_table, common_key):
+        joined_table = Table(self.table_name + '_joins_' + other_table.table_name, [])
+        for item1 in self.table:
+            for item2 in other_table.table:
+                if item1[common_key] == item2[common_key]:
+                    dict1 = copy.deepcopy(item1)
+                    dict2 = copy.deepcopy(item2)
+                    dict1.update(dict2)
+                    joined_table.table.append(dict1)
+        return joined_table
+    
+    def filter(self, condition):
+        filtered_table = Table(self.table_name + '_filtered', [])
+        for item1 in self.table:
+            if condition(item1):
+                filtered_table.table.append(item1)
+        return filtered_table
+    
+    def aggregate(self, function, aggregation_key):
+        temps = []
+        for item1 in self.table:
+            temps.append(float(item1[aggregation_key]))
+        return function(temps)
+    
+    def select(self, attributes_list):
+        temps = []
+        for item1 in self.table:
+            dict_temp = {}
+            for key in item1:
+                if key in attributes_list:
+                    dict_temp[key] = item1[key]
+            temps.append(dict_temp)
+        return temps
 
-# Print the max temperature for all the cities in Italy
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The max temperature of all the cities in", my_country, ":")
-print(max(temps))
-print()
-
-# Print the min temperature for all the cities in Italy
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The min temperature of all the cities in", my_country, ":")
-print(min(temps))
-print()
-
-# Let's write a function to filter out only items that meet the condition
-# Hint: condition will be associated with an anonymous function, e.x., lamdbda x: max(x)
-def filter(condition, dict_list):
-    filtered_list = []
-    for item in dict_list:
-        if condition(item):
-            filtered_list.append(item)
-    return filtered_list
-
-x = filter(lambda x: float(x['latitude']) >= 60.0, cities)
-for item in x:
-    print(item)
-
-# Let's write a function to do aggregation given an aggregation function and an aggregation key
-def aggregate(aggregation_key, aggregation_function, dict_list):
-    values = [float(item[aggregation_key]) for item in dict_list]
-    return aggregation_function(values)
-
-# Let's write code to
-# - print the average temperature for all the cities in Italy
-# - print the average temperature for all the cities in Sweden
-# - print the min temperature for all the cities in Italy
-# - print the max temperature for all the cities in Sweden
-avg_temp_italy = aggregate('temperature', lambda x: sum(x)/len(x), filter(lambda x: x['country'] == 'Italy', cities))
-print("The average temperature for all the cities in Italy:")
-print(avg_temp_italy)
-print()
-
-
-avg_temp_sweden = aggregate('temperature', lambda x: sum(x)/len(x), filter(lambda x: x['country'] == 'Sweden', cities))
-print("The average temperature for all the cities in Sweden:")
-print(avg_temp_sweden)
-print()
-
-
-min_temp_italy = aggregate('temperature', min, filter(lambda x: x['country'] == 'Italy', cities))
-print("The min temperature for all the cities in Italy:")
-print(min_temp_italy)
-print()
-
-
-max_temp_sweden = aggregate('temperature', max, filter(lambda x: x['country'] == 'Sweden', cities))
-print("The max temperature for all the cities in Sweden:")
-print(max_temp_sweden)
-print()
+    def __str__(self):
+        return self.table_name + ':' + str(self.table)
